d46dc4e0624ea5844a13d02b10519eb6b80a2c69
MusicalWhiskey <124202980+MusicalWhiskey@users.noreply.github.com>
2024-10-22 21:33:00 -0700

    links to pages

--- index.js
+++ index.js
@@ -10,10 +10,26 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/static', express.static('public'));
 
 
-
 console.log("ALAB 318.2.1");
 
-
+app.get('/', (req, res) => {
+    res.render('main');
+  });
+  
+  app.get('/info', (req, res) => {
+    res.render('info');
+  });
+  
+  app.post('/submit', (req, res) => {
+    console.log(req.body.data);
+    res.send('Success');
+  });
+  
+  // Route with parameter
+  app.get('/user/:name', (req, res) => {
+    const userName = req.params.name;
+    res.send(`Hello, ${userName}!`);
+  });
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
