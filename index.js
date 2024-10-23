3030d5338ac103b94e1548f4973e777903c71b7a
MusicalWhiskey <124202980+MusicalWhiskey@users.noreply.github.com>
2024-10-22 21:25:18 -0700

    progress

--- index.js
+++ index.js
@@ -2,4 +2,19 @@ const express = require("express")
 const app = express()
 const PORT = 3000;
 
-console.log("Hello World");
+app.set("view engine", "ejs");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/static', express.static('public'));
+
+
+
+console.log("ALAB 318.2.1");
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
